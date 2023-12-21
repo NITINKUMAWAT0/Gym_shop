7867cc41d0093ed47d2f5963161fa4c985c6a00b
NITINKUMAWAT0 <nitin.kumawat.jaipur.in@gmail.com>
2023-12-21 11:12:17 +0530

    installed strapi into the project...!!

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,42 @@
 import React from 'react';
-import {createBrowserRouter , RouterProvider , Outlet} from "react-router-dom"
+import { BrowserRouter, Routes, Route ,Outlet} from 'react-router-dom'; // Corrected imports
+
+// Pages
+import Home from './pages/Home';
+import Products from './pages/Products';
+import ProductDetails from './pages/ProductDetails';
+import Search from './pages/Search';
+
+// Components
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+// Layout
+const Layout = () => { 
+  return (
+    <div>
+      <Header />
+      <main> 
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} /> 
+          <Route path="products/:id" element={<Products />} />
+          <Route path="product/:id" element={<ProductDetails />} />
+          <Route path="search" element={<Search />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
